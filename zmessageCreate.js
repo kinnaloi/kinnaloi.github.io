c114845545b7a95bcf1ed99f63513b7e01ce362c
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-12-17 21:52:40 +0600

    uploading ;

--- zmessageCreate.js
+++ zmessageCreate.js
@@ -24,5 +24,33 @@ fetch("./database/database.json")
 //message grab from page
 
 function buyNow() {
-    
+  var idNumber = document.querySelector(".idValue").value;
+  var item = document.querySelector(".itemValue").value;
+  var trxnId = document.querySelector(".transectionValue").value;
+  var phoneNumber = document.querySelector(".phoneValue").value;
+  var mobileBank = document.querySelector(".mobileBanking").value;
+
+  var waLink = "https://wa.me/";
+  var myPhone = "+8801755267077";
+  var text =
+    "Hey Please review it " +
+    "%0a" +
+    "Player UID : " +
+    idNumber +
+    "%0a" +
+    "*Item* : " +
+    item +
+    "%0a" +
+    "Phone Number : " +
+    phoneNumber +
+    "%0a" +
+    "Transection ID : " +
+    trxnId +
+    "%0a" +
+    "Path : " +
+    mobileBank +
+    "%0a";
+  let url = waLink + myPhone + "?text=" + text;
+  alert("Processing ...\n Press Continue to Chat");
+  window.open(url, "_blank");
 }
